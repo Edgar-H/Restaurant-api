41fe072a3389b9696ff14d6758a8bae6613ad8e8
Billy <fx4038@gmail.com>
2022-10-22 09:44:18 -0400

    deploy-6

--- src/config.js
+++ src/config.js
@@ -4,6 +4,5 @@ module.exports = {
     port: process.env.PORT || 5000,
     node_env: process.env.NODE_ENV || 'development',
     jwtSecret: process.env.JWT_SECRET,
-    domainHost: process.env.DOMAIN_HOST || "http://localhost:"
-
+    domainHost: process.env.DOMAIN_HOST || 'http://localhost:',
 };
